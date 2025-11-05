1ab2d8153f567ef2249aabedbfcc2d59f7fa9ae3
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-11-05 15:37:45 +0500

    Add postgres

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,25 @@
 import express from "express"
 import cors from "cors"
+import { Pool } from "pg"
+
+const pool = new Pool( {
+	user: "postgres",
+	password: "math",
+	host: "localhost",
+	port: 5432,
+	database: "postgres",
+} )
 
 const app = express()
 
 app.use( cors() )
 
 app.get( "/health", ( _ ,res ) => res.send( { message: "OK" } ) )
-app.get( "/some-number", ( _ ,res ) => res.send( [ 1 ] ) )
+app.get( "/some-number", async ( _ ,res ) => {
+
+	const data = await pool.query( "SELECT 1 number" )
+
+	res.send( data.rows )
+} )
 
 app.listen( 3_000, () => console.info( ":3000" ) )
