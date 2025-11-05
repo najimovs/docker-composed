d8a3be3311c095a5cb422c61a002b2a21256827e
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-11-05 15:30:56 +0500

    Load data from backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express"
+import cors from "cors"
 
 const app = express()
+
+app.use( cors() )
 
 app.get( "/health", ( _ ,res ) => res.send( { message: "OK" } ) )
 app.get( "/some-number", ( _ ,res ) => res.send( [ 1 ] ) )
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,1 +1,10 @@
 import "@app/main.css"
+
+load()
+
+async function load() {
+
+	const json = await ( await fetch( "http://localhost:3000/some-number" ) ).json()
+
+	console.log( json )
+}
